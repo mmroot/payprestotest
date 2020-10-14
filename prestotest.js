b7cd6677f387e588028f043fb57bcac697dd6e7f
mmroot <razvan.petre@gmail.com>
2020-10-14 21:14:28 +0200

    paypresto test

---
+++ prestotest.js
@@ -0,0 +1,33 @@
+const bsv = require('bsv');
+const Presto = require('paypresto.js');
+
+let privateKey = bsv.PrivKey.fromRandom();
+
+let publicKey = bsv.PubKey.fromPrivKey(privateKey);
+let address = bsv.Address.fromPubKey(publicKey);
+
+console.log('PRIVATE KEY: ', privateKey.toWif());
+console.log('PUBLIC KEY:  ', publicKey.toHex());
+console.log('To ADDRESS:  ', address.toString());
+
+const payment = Presto.create({
+  // Provide WIF key, or bsv2 PrivKey or KeyPair instance
+  key: privateKey.toWIF(),
+
+  // Short human readable description of payment
+  description: 'My Paypresto test payment',
+
+  // An array of outputs, can include simple p2pkh outputs,
+  // custom scripts or embedded data
+  outputs: [
+    { to: '1PqRhzVabqJqnvjNy8nb9CD3amT8NkRmJH', satoshis: 5000 },
+    { script: '76a91412ab8dc588ca9d5787dde7eb29569da63c3a238c88ac', satoshis: 5000 },
+    { data: [Buffer.from("Hello world!")] }
+  ]
+});
+
+payment
+  .on('invoice', invoice => console.log("Invoiced...") /* fired when invoice created */)
+  .on('funded', payment => payment.pushTx())
+  .on('success', txid => console.log("*success", txid)/* fired when tx successfully sent */)
+  .on('error', err => console.log("*Error ", err)/* fired when error is encountered */)
